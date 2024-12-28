60388e850bcb9eee546d205aa06144d2c14d9b36
Udara Nalawansa <udarakavishka13@gmail.com>
2024-12-28 09:23:48 +0530

    refactor: Replace View with Fragment in Home component

--- MyMobileApp/components/Home.js
+++ MyMobileApp/components/Home.js
@@ -3,7 +3,7 @@ import { PaperProvider, Text, Divider } from 'react-native-paper';
 
 export default function Home() {
     return (
-        <View>
+        <>
             <Text variant="headlineLarge">Headline Large</Text>
             <Divider />
             <Text variant="bodyMedium" style={styles.body}>
@@ -15,7 +15,7 @@ export default function Home() {
 
                 Extremity direction existence as dashwoods do up. Securing marianne led welcomed offended but offering six raptures. Conveying concluded newspaper rapturous oh at. Two indeed suffer saw beyond far former mrs remain. Occasional continuing possession we insensible an sentiments as is. Law but reasonably motionless principles she. Has six worse downs far blush rooms above stood.
             </Text>
-        </View>
+        </>
 
     );
 }

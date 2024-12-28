13debb3814e2eacb8b275e43942d9a4305bcf4b5
Udara Nalawansa <udarakavishka13@gmail.com>
2024-12-28 12:59:08 +0530

    feat: Refactor App component to include KeyboardAvoidingView and SafeAreaProvider

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -1,23 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import Home from './components/Home';
 import ContactUs from './components/ContactUs';
 
 export default function App() {
   return (
     <PaperProvider>
-    <SafeAreaView>
-    <ScrollView>
-        <View style={styles.container}>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <ScrollView >
+              <View style={styles.container}>
+                <ContactUs />
+                <StatusBar style="auto" />
+              </View>
+            </ScrollView>
+          </KeyboardAvoidingView>
 
-          <ContactUs />
-          <StatusBar style="auto" />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-      
+        </SafeAreaView>
+      </SafeAreaProvider>
+
+
 
     </PaperProvider>
   );

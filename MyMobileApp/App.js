48be328ec2a04a331c9554cc978815e7fd01503a
Udara Nalawansa <udarakavishka13@gmail.com>
2024-12-28 09:14:23 +0530

    feat: Add body text and styling to App component

--- MyMobileApp/App.js
+++ MyMobileApp/App.js
@@ -8,3 +8,32 @@ export default function App() {
       <View style={styles.container}>
         <Text variant="headlineLarge">Headline Large</Text>
         <Divider />
+        <Text variant="bodyMedium" style={styles.body}>
+        For norland produce age wishing. To figure on it spring season up. Her provision acuteness had excellent two why intention. As called mr needed praise at. Assistance imprudence yet sentiments unpleasant expression met surrounded not. Be at talked ye though secure nearer.
+
+          No opinions answered oh felicity is resolved hastened. Produced it friendly my if opinions humoured. Enjoy is wrong folly no taken. It sufficient instrument insipidity simplicity at interested. Law pleasure attended differed mrs fat and formerly. Merely thrown garret her law danger him son better excuse. Effect extent narrow in up chatty. Small are his chief offer happy had.
+
+          Attended no do thoughts me on dissuade scarcely. Own are pretty spring suffer old denote his. By proposal speedily mr striking am. But attention sex questions applauded how happiness. To travelling occasional at oh sympathize prosperous. His merit end means widow songs linen known. Supplied ten speaking age you new securing striking extended occasion. Sang put paid away joy into six her.
+
+          Extremity direction existence as dashwoods do up. Securing marianne led welcomed offended but offering six raptures. Conveying concluded newspaper rapturous oh at. Two indeed suffer saw beyond far former mrs remain. Occasional continuing possession we insensible an sentiments as is. Law but reasonably motionless principles she. Has six worse downs far blush rooms above stood.
+          </Text>
+          
+        <StatusBar style="auto" />
+      </View>
+    </PaperProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  body: {
+    padding: 15,
+    textAlign: 'justify',
+
+  }
+});

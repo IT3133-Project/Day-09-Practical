8672353da0bb9dcf0616cf1388ec4adea6bbd7a9
Udara Nalawansa <udarakavishka13@gmail.com>
2024-12-28 12:26:27 +0530

    feat: Enhance ContactUs component with input fields for user information

--- MyMobileApp/components/ContactUs.js
+++ MyMobileApp/components/ContactUs.js
@@ -1,20 +1,31 @@
-import { StyleSheet ,View} from 'react-native';
-import {Text, Divider } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Text, Divider, TextInput } from 'react-native-paper';
 import Home from './Home';
 
 
 export default function ContactUs() {
-    return(
+    return (
         <>
             <View style={styles.header}>
-            <Text variant="headlineLarge">Contact Us</Text>
-            <Divider />
+                <Text variant="headlineLarge">Contact Us</Text>
+                <Divider />
             </View>
             <View style={styles.body}>
-            
+                <View style={styles.input}>
+                    <TextInput label="Name" mode='outlined' />
+                </View>
+                <View style={styles.input}>
+                    <TextInput label="Email" mode='outlined' keyboardType='email-address'/>
+                </View>
+                <View style={styles.input}>
+                    <TextInput label="Phone Number" mode='outlined' keyboardType='phone-pad'/>
+                </View>
+                <View style={styles.input}>
+                    <TextInput label="Message" mode='outlined' multiline numberOfLines={5}/>
+                </View>
             </View>
             <View style={styles.footer}>
-            <Text>MyApp © 2024</Text>
+                <Text>MyApp © 2024</Text>
             </View>
         </>
     );
@@ -22,20 +33,28 @@ export default function ContactUs() {
 
 
 const styles = StyleSheet.create({
-  header: {
-    flex:3,
-    width:"100%",
-    alignItems:"center",
-  },
-  body:{
-    flex:5,
-    width:"100%",
-    alignItems:"center",
-  },
-  footer:{
-    flex:2,
-    width:"100%",
-    alignItems:"center",
-  }
-  
+    header: {
+        flex: 3,
+        width: "100%",
+        alignItems: "center",
+        padding: 10,
+        marginBottom: 10,
+    },
+    body: {
+        flex: 5,
+        width: "100%",
+    },
+    footer: {
+        flex: 2,
+        width: "100%",
+        alignItems: "center",
+        marginTop: 10,
+    },
+    input: {
+        padding: 5,
+        marginBottom: 5,
+
+    }
+
+
 });
